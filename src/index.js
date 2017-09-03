a78d0ce8f966ec34251ae5850b8fef44c09e5f79
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2017-09-04 01:17:56 +0800

    Add third arm

--- src/index.js
+++ src/index.js
@@ -12,37 +12,47 @@ document.body.appendChild( renderer.domElement );
 const cuboid = new THREE.BoxGeometry( 2, 0.25, 0.25 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00,
                                                 wireframe: true } );
+
 const upperArm = new THREE.Mesh( cuboid, material );
+const middleArm = new THREE.Mesh( cuboid, material );
 const lowerArm = new THREE.Mesh( cuboid, material );
 
+const base = new THREE.Object3D();
 const shoulder = new THREE.Object3D();
 const elbow = new THREE.Object3D();
 
-shoulder.add( upperArm );
-upperArm.add( elbow );
+base.add( upperArm );
+upperArm.add( shoulder );
+shoulder.add( middleArm );
+middleArm.add( elbow );
 elbow.add( lowerArm );
-scene.add( shoulder );
 
-camera.position.y = 1.5;
+scene.add( base );
+
+camera.position.y = 2.2;
 camera.position.z = 5;
 
 upperArm.position.x = 1;
+shoulder.position.x = 1;
+middleArm.position.x = 1;
 elbow.position.x = 1;
 lowerArm.position.x = 1;
-shoulder.rotation.z = Math.PI / 4;
+
+base.rotation.z = Math.PI / 4;
 
 let delta = 0.05;
 
 const animate = () => {
     requestAnimationFrame( animate );
 
-    if ( shoulder.rotation.z < Math.PI / 4 ||
-         shoulder.rotation.z > Math.PI * 2 / 3 )
+    if ( base.rotation.z < Math.PI / 4 ||
+         base.rotation.z > Math.PI * 2 / 3 )
     {
         delta = -delta;
     }
+    base.rotation.z += delta;
     shoulder.rotation.z += delta;
-    elbow.rotation.z += 0.5;
+    elbow.rotation.z += delta;
 
     renderer.render(scene, camera);
 };
