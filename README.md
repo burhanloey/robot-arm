8265007e05026011056144776bf38cb48a1d96c2
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2017-09-04 20:37:31 +0800

    Add readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# robot-arm
+
+Robot arm movement using jointed spherical model.
+
+Control the robot using W, A, S, D, Up and Down buttons.
+
+## License
+
+The MIT License
--- src/components/materials.js
+++ src/components/materials.js
@@ -4,4 +4,4 @@ export const green = new THREE.MeshBasicMaterial( { color: 0x00ff00,
                                                     wireframe: true } );
 
 export const blue = new THREE.MeshBasicMaterial( { color: 0x0000ff,
-                                                   wireframe: true} );
+                                                   wireframe: true } );
