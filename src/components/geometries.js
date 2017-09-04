bcb9e4933b6bd58f9958793d4524eb10b704fb48
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2017-09-04 20:55:15 +0800

    Use BufferGeometry

--- src/components/geometries.js
+++ src/components/geometries.js
@@ -1,10 +1,10 @@
 import * as THREE from 'three';
 
-export const cuboid = new THREE.BoxGeometry( 0.25, 2, 0.25 );
+export const cuboid = new THREE.BoxBufferGeometry( 0.25, 2, 0.25 );
 
-export const flatCuboid = new THREE.BoxGeometry( 1, 0.1, 1 );
+export const flatCuboid = new THREE.BoxBufferGeometry( 1, 0.1, 1 );
 
-export const shortCuboid = new THREE.BoxGeometry( 0.25, 0.5, 0.25 );
+export const shortCuboid = new THREE.BoxBufferGeometry( 0.25, 0.5, 0.25 );
 
-export const cylinder = new THREE.CylinderGeometry( 0.3, 0.3, 0.4, 16 );
+export const cylinder = new THREE.CylinderBufferGeometry( 0.3, 0.3, 0.4, 16 );
 cylinder.rotateX( Math.PI / 2 );
